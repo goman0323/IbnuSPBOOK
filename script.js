6c9cf3ecbe15fa2b62b42b915737b0c6d6056e0d
goman0323 <ivnu.ceena2000@gmail.com>
2025-11-02 16:35:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
-    const text = "Apa yang di butuhkan, pake aja ya wak";
-    const typedText = document.getElementById("typedText");
+    // Efek ketik
+    const text = "Pahami • Ingat • Coba.";
     let i = 0;
-    let deleting = false;
 
     function typeWriter() {
-      if (!deleting) {
-        if (i < text.length) {
-          typedText.textContent += text.charAt(i);
-          i++;
-          setTimeout(typeWriter, 100); // Delay antar karakter saat mengetik
-        } else {
-          deleting = true;
-          setTimeout(typeWriter, 1000); // Tunggu sebentar sebelum menghapus
-        }
-      } else {
-        if (i > 0) {
-          typedText.textContent = text.substring(0, i - 1);
-          i--;
-          setTimeout(typeWriter, 50); // Delay antar karakter saat menghapus
-        } else {
-          deleting = false;
-          setTimeout(typeWriter, 1000); // Tunggu sebentar sebelum mengetik lagi
-        }
+      if (i < text.length) {
+        document.getElementById("typedText").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 60);
       }
     }
 
-    typeWriter();
-
-    // Modal delay tampil
-    setTimeout(function() {
-      document.getElementById("myModal").style.display = "block";
-    }, 500);
+    // Modal auto tampil
+    window.onload = function() {
+      setTimeout(() => {
+        document.getElementById("myModal").style.display = "block";
+      }, 1200);
+      typeWriter();
+    }
 
     function closeModal() {
       document.getElementById("myModal").style.display = "none";
